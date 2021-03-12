991bd39b88485c04ee6e3c59474d7dc94b0e4f05
saikirantokala <saikiran.tokala@digitap.ai>
2021-03-12 15:47:38 +0530

    0.1.9 Test version

---
+++ DTOkyc.framework/Headers/DTOkyc.h
@@ -0,0 +1,18 @@
+//
+//  DTOkyc.h
+//  DTOkyc
+//
+//  Created by Sai Kiran on 03/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DTOkyc.
+FOUNDATION_EXPORT double DTOkycVersionNumber;
+
+//! Project version string for DTOkyc.
+FOUNDATION_EXPORT const unsigned char DTOkycVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkSample/PublicHeader.h>
+
+
